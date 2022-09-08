12f41c23cdd2bb40d4050d9dc99c016812433fef
J0kk3 <joakim_88@msn.com>
2022-09-08 17:04:23 +0200

    [BUG] fixed bugs

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App ()
     setError( null );
     try
     {
-      const response = await fetch( 'https://react-http-8f423-default-rtdb.europe-west1.firebasedatabase.app/movies.json' );
+      const response = await fetch( 'https://react-http-6b4a6.firebaseio.com/movies.json' );
       if ( !response.ok )
       {
         throw new Error( 'Something went wrong!' );
--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -2,14 +2,12 @@ import React, { useRef } from 'react';
 
 import classes from './AddMovie.module.css';
 
-function AddMovie ( props )
-{
-  const titleRef = useRef( '' );
-  const openingTextRef = useRef( '' );
-  const releaseDateRef = useRef( '' );
+function AddMovie(props) {
+  const titleRef = useRef('');
+  const openingTextRef = useRef('');
+  const releaseDateRef = useRef('');
 
-  function submitHandler ( event )
-  {
+  function submitHandler(event) {
     event.preventDefault();
 
     // could add validation here...
@@ -20,22 +18,22 @@ function AddMovie ( props )
       releaseDate: releaseDateRef.current.value,
     };
 
-    props.onAddMovie( movie );
+    props.onAddMovie(movie);
   }
 
   return (
-    <form onSubmit={ submitHandler }>
-      <div className={ classes.control }>
+    <form onSubmit={submitHandler}>
+      <div className={classes.control}>
         <label htmlFor='title'>Title</label>
-        <input type='text' id='title' ref={ titleRef } />
+        <input type='text' id='title' ref={titleRef} />
       </div>
-      <div className={ classes.control }>
+      <div className={classes.control}>
         <label htmlFor='opening-text'>Opening Text</label>
-        <textarea rows='5' id='opening-text' ref={ openingTextRef }></textarea>
+        <textarea rows='5' id='opening-text' ref={openingTextRef}></textarea>
       </div>
-      <div className={ classes.control }>
+      <div className={classes.control}>
         <label htmlFor='date'>Release Date</label>
-        <input type='text' id='date' ref={ releaseDateRef } />
+        <input type='text' id='date' ref={releaseDateRef} />
       </div>
       <button>Add Movie</button>
     </form>
